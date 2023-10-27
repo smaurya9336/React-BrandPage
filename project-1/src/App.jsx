320e865292df94cca9b89cf08f6cc67bf7cc92f6
Sarita <sm7767119@gmail.com>
2023-10-27 18:51:13 +0530

    React-10-project

---
+++ project-1/src/App.jsx
@@ -0,0 +1,13 @@
+import Hero from "./Components/Hero";
+import Navigation from "./Components/Navigation";
+const App = () => {
+    return( 
+    <div>
+    <Navigation />
+    <Hero />
+    </div>
+
+   );
+    
+}
+export default App;
---
+++ project-1/src/Components/Hero.jsx
@@ -0,0 +1,28 @@
+const Hero=()=>{
+    return<main className="hero container">
+     <div className="hero-content">
+        <h1>YOUR FEET DESERVE THE BEST</h1>
+        <p>YOUR FEET DESERVE THE BEST AND WHERE HERE TO HELP YOU WITH OUR 
+        SHOES. YOUR FEET DESERVE THE BEST AND WHERE HERE TO HELP YOU WITH OUR SHOES. </p>
+     
+     <div className="hero-btn">
+        <button>Shop Now</button>
+        <button className="secondary-btn">Category</button>
+     </div>
+
+     <div className="shopping">
+        <p>Also Available On</p>
+
+        <div className="brand-icon">
+           <img src="/images/flipkart-logo.png" height="30px" alt="" />
+            <img src="/images/amazon-logo.png" height="30px" alt="" />
+        </div>
+     </div>
+    </div>
+
+     <div className="hero-image">
+        <img src="/images/nike-shoes.png" height="400px" alt="hero-image" />
+     </div>
+    </main>
+}
+export default Hero;
